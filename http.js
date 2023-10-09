519631e6eb74b7dac220afd4b7374be866757b10
Contreraswz <contreraswz@gmail.com>
2023-10-09 16:17:15 -0600

    final modulo 1

--- http.js
+++ http.js
@@ -1,4 +1,14 @@
 const http = require('node:http')
-const server = http.createServer(req,res)=>{
-     
-}
+const { findAvailablePort } = require('./free-port.js')
+
+const desiredPort = process.env.PORT ?? 3000
+const server = http.createServer((req, res) => { // Crea la constante de servidor, el servidor solo puede hacer dos cosas "recibir petición o enviar respuesta"
+  console.log('request recived')
+  res.end('Hola')
+})
+
+findAvailablePort(desiredPort).then(port => {
+  server.listen(port, () => { // Escucha en el puerto disponible
+    console.log(`Sevidor escuchando en el puerto http://localhost:${server.address().port}`)
+  })
+})
